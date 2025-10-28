d6483e55cef78da58ac8e38e6c7a0e0dc4b2ff14
josevitor555 <koservitor123@gmail.com>
2025-10-27 21:43:33 -0300

    Alteração dia 27/10/2025

--- backend/taskApp/views.py
+++ backend/taskApp/views.py
@@ -1,10 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 #  Função para cadastrar usuário
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, template_name='cadastro.html')
+    else:
+        username = request.POST.get('username')
+
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        
+        # Verificação de campos vazios
+        if not username or not email or not senha:
+            return HttpResponse("Todos os campos são obrigatórios.".encode('utf-8'), content_type="text/plain; charset=utf-8")
+        
+        # Aqui você pode adicionar a lógica para salvar o usuário no banco de dados
+        # Exemplo: User.objects.create_user(username=username, email=email, password=senha)
+        
+
+
+        return HttpResponse(username.encode('utf-8'), content_type="text/plain; charset=utf-8")      
 
 #  Função para tela de login
 def login(request):
